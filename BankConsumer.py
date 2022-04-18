cf5bfff04d26ebd9d0fb681f11ee12915753a767
Shivakumarbandari <103282975+Shivakumarbandari@users.noreply.github.com>
2022-04-18 18:56:48 +0530

    Add files via upload

---
+++ BankConsumer.py
@@ -0,0 +1,22 @@
+# from BankConsumer import BankInfo as BNK
+class Account:
+    def __init__(self, balance=0.0, name=''):
+        self.balance = balance
+        self.name = name
+        self.nominee = None
+    def Deposit(self, balance=0.0):
+        self.balance = self.balance + balance
+        print("Deposit is succesfull and the balance in account is %f" % self.balance)
+
+    def Withdrawal(self, balance=0.0):
+        if (self.balance >= balance):
+
+            self.balance = self.balance - balance
+            print("withdraw is successfull and balance is %f" % self.balance)
+
+        else:
+            print("Insufficient balance")
+
+    def AddNominee(self, name=''):
+        self.nominee = name
+        print(self.nominee)
